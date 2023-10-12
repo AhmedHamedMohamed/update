f617906af583a6cdd6b17664f092b48501badd50
AhmedHamedMohamed <130000845+AhmedHamedMohamed@users.noreply.github.com>
2023-10-12 09:39:05 +0300

    Add files via upload

---
+++ src/test/java/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.testng.annotations.Test;
+public class test {
+
+    @Test
+    public void testing(){
+        System.out.println("ahmed hamed");
+    }
+}
